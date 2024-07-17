8db5b71f5664b2f1f8cf8bc75a780e2b66ebeca4
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-07-17 23:48:10 +0530

    basic flatlist intro

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <FlatList
+        data={[{ id: 1 }, { id: 1 }, { id: 1 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item, index }) => (
+          <Text key={index} className="text-3xl">
+            {item.id}
+          </Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6"> 
+            <View>
+              <Text>Welcome Back</Text>
+              <Text>Again here</Text>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
 
 export default Home
